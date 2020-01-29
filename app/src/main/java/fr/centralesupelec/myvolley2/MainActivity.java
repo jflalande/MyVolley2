6df0d04ea61cd637f6ebb8d05286e8b74994018e
Jean-Francois Lalande <jean-francois.lalande@irisa.fr>
2020-01-29 18:55:34 +0100

    Add comments.

--- app/src/main/java/fr/centralesupelec/myvolley2/MainActivity.java
+++ app/src/main/java/fr/centralesupelec/myvolley2/MainActivity.java
@@ -20,15 +20,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Asks to the singleton class MySingleton to send back the RequestQueue
         RequestQueue queue = MySingleton.getInstance(this.getApplicationContext()).
                 getRequestQueue();
-
-// ...
 
         ImageRequest ir = new ImageRequest("https://helpx.adobe.com/content/dam/help/en/stock/how-to/visual-reverse-image-search/jcr_content/main-pars/image/visual-reverse-image-search-v2_intro.jpg", new Response.Listener<Bitmap>() {
             @Override
             public void onResponse(Bitmap response) {
                 // callback
+                // Here you should code what has to be done
+                // with the obtaine image from Volley
+                // In this example, I change an imageView
+                // For a ListView, you should add this image
+                // in the ArrayAdapter
                 Log.i("JFL", "I got an answer !");
                 Log.i("JFL", ""+ response.toString());
                 ImageView iv = (ImageView)findViewById(R.id.imageView);
@@ -36,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }, 400, 300, null, null);
 
-        // Add a request (in this example, called stringRequest) to your RequestQueue.
+        // Add a request to the queue
         MySingleton.getInstance(this).addToRequestQueue(ir);
 
 
